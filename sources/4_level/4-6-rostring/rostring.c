0b3d09c5ae17c775c102da55a79a6a4d29122f62
Andrew <a.galavan@icloud.com>
2018-01-29 21:42:56 +0200

    rostring updated

--- sources/4_level/4-6-rostring/rostring.c
+++ sources/4_level/4-6-rostring/rostring.c
@@ -10,19 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
 
 void	rostring(char *s)
 {
-	while (*s)
+	int		i;
+	int		j;
+
+	i = 0;
+	while (s[i] && (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'))
+		i++;
+	j = i;
+	while (s[i] && s[i] != ' ' && s[i] != '\n' && s[i] != '\t')
+		i++;
+	while (s[i])
 	{
-		
+		while (s[i] && (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'))
+			i++;
+		while (s[i] && s[i] != ' ' && s[i] != '\n' && s[i] != '\t')
+		{
+			write(1, &s[i], 1);
+			i++;
+		}
+		if (s[i] || (s[i - 1] != ' ' && s[i - 1] != '\n' && s[i - 1] != '\t'))
+			write(1, " ", 1);
 	}
+	while (s[j] && s[j] != ' ' && s[j] != '\n' && s[j] != '\t')
+		write(1, &s[j++], 1);
 }
 
 int		main(int argc, char **argv)
 {
-	if (argc == 2)
+	if (argc >= 2)
 	{
 		rostring(argv[1]);
 	}
